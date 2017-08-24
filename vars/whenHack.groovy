33a46365f4b45306af504b51b3ad23fc205239ae
Joe Scaduto <jscaduto@mysidewalk.com>
2017-08-24 16:37:19 -0500

    add whenHack

---
+++ vars/whenHack.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+// until this issues gets resolved https://issues.jenkins-ci.org/browse/JENKINS-44053...
+
+void call(boolean conditional) {
+  if ( !conditional ) {
+    echo 'Stage skipped due to when conditional'
+    return
+  }
+}
