b70aa1e5090ff8eb5830db680f7efc59d430189a
Joe Scaduto <jscaduto@mysidewalk.com>
2017-08-24 13:06:11 -0500

    stop shadowing deploymentType, change returned var to result

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 
-String call(String deploymentType, String branchName) {
+String call(String action, String branchName) {
   BRANCH_TO_ENVIRONMENT = [
     (branch.EDGE): environment.EDGE,
     (branch.MASTER): environment.STAGE,
   ]
-  if (deploymentType in [deploymentType.PROD_DEPLOY, deploymentType.PROD_PREDEPLOY]) {
+  if (action in [deploymentType.PROD_DEPLOY, deploymentType.PROD_PREDEPLOY]) {
     result = environment.PROD
   }
   else {
-    result = BRANCH_TO_ENVIRONMENT[$branchName] ?: environment.LOCAL
+    result = BRANCH_TO_ENVIRONMENT[branchName] ?: environment.LOCAL
   }
   result
 }
