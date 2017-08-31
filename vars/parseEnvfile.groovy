6449f171a182261a3966e5f7f05780b9f9b4a451
Joe Scaduto <jscaduto@mysidewalk.com>
2017-08-31 17:22:41 -0500

    parseEnvfile with ignorecase

--- vars/parseEnvfile.groovy
+++ vars/parseEnvfile.groovy
@@ -1,5 +1,5 @@
 #!/usr/bin/env groovy
 
 String call(String property, String envfile) {
-  sh(script: "grep ${property}= ${envfile}", returnStdout: true).tokenize('=')[1].trim()
+  sh(script: "grep -i ${property}= ${envfile}", returnStdout: true).tokenize('=')[1].trim()
 }
