a8d59dfac34695334b7e941f373b0e3c4cb0a7f1
Joe Scaduto <jscaduto@mysidewalk.com>
2017-10-17 13:01:57 -0500

    add service and parameter groovy vars

---
+++ vars/service.groovy
@@ -0,0 +1,10 @@
+#!/usr/bin/env groovy
+
+class service implements Serializable {
+  private static String AUTHWALK = 'authwalk'
+  private static String ELECTIONS = 'elections'
+  private static String FRONTEND = 'frontend'
+  private static String MYSIDEWALK = 'mysidewalk'
+  private static String TESSERACT = 'tesseract'
+  private static List<String> ALL = [AUTHWALK, ELECTIONS, FRONTEND, MYSIDEWALK, TESSERACT]
+}
