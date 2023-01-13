372bf5cbbb33acde833621323bf134f0656c3f06
Tony Ebel <tebel@mysidewalk.com>
2023-01-13 14:49:27 -0600

    alter addNpmTokens function to parse envfile

--- vars/addNpmTokens.groovy
+++ vars/addNpmTokens.groovy
@@ -1,18 +1,17 @@
 #!/usr/bin/env groovy
 
-void call(String etcdPrefix) {
+void call(String envfile) {
   sh """
     set -o errexit
     set -o nounset
     set -o pipefail
 
-    export ETCDCTL_API=2
-    export ETCDCTL_ENDPOINTS='http://config-1:4001,http://config-2:4001,http://config-3:4001'
-    npm_tokens=\$(etcdctl ls ${etcdPrefix} | rev | cut -d '/' -f 1 | rev)
+    npm_tokens=\$(grep -P '^npm\.' ${envfile})
 
     if [[ -n \${npm_tokens} ]]; then
-      for token_name in \${npm_tokens[@]}; do
-        token_value=\$(etcdctl get ${etcdPrefix}/\${token_name})
+      for token in \${npm_tokens[@]}; do
+        token_name=\$(echo \${token} | cut -d '=' -f 1)
+        token_value=\$(echo \${token} | cut -d '=' -f 2)
         echo "//\${token_name}/:_authToken=\${token_value}" >> .npmrc
       done
     fi
