bcedada8d323f593d6d434cca3fb482bb238d939
Tony Ebel <tebel@mysidewalk.com>
2023-03-23 11:15:45 -0500

    breaking up getNpmTokens into it's own function as it can't run in the same stage as addNpmToken

--- vars/addNpmTokens.groovy
+++ vars/addNpmTokens.groovy
@@ -1,19 +1,12 @@
 #!/usr/bin/env groovy
 
-void call(String environment, String service) {
+void call(String service) {
   sh """
     set -o errexit
     set -o nounset
     set -o pipefail
 
-    docker run --rm \
-      -v \$(pwd):/output \
-      -e ENVIRONMENT=${environment} \
-      -e SERVICE=${service} \
-      -e CONFD_NODES='["http://config-1:2379", "http://config-2:2379", "http://config-3:2379"]' \
-      gcr.io/mindmixer-sidewalk/confd:envfile
-
-    npm_tokens=\$(grep -iP '^npm\.' ${service}.env)
+    npm_tokens=\$(grep -iP '^npm\\.' ${service}.env)
 
     if [[ -n \${npm_tokens} ]]; then
       for token in \${npm_tokens[@]}; do
