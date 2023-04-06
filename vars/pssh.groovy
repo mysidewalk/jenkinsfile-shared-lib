c39d3dc2a2f1dcd492a593a876b77c3f193d4564
Tony Ebel <tebel@mysidewalk.com>
2023-04-06 13:20:44 -0500

    pssh rewrite to use bash background processes and ssh

--- vars/pssh.groovy
+++ vars/pssh.groovy
@@ -2,12 +2,27 @@
 
 void call(Set<String> hosts, String command) {
   sh """
-    pssh \
-      -H "${hosts.sort().join(' ')}" \
-      -O StrictHostKeyChecking=no \
-      -O UserKnownHostsFile=/dev/null \
-      -t 300 \
-      --inline \
-      "${command}"
+    hosts="${hosts.sort().join(' ')}"
+    tmpdir=/tmp/pssh.\$\$
+    mkdir -p \$tmpdir
+
+    for host in \${hosts[@]}; do
+      timeout 300 ssh \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        -o ConnectTimeout=10 \
+        -o BatchMode=yes \
+        \${host} \
+        "${command}" > \${tmpdir}/\${host} 2>&1 &
+    done
+    
+    wait
+    
+    for host in \${hosts[@]}; do
+      printf "\n\n#################\nOutput: \${host}\n#################\n"
+      cat \${tmpdir}/\${host}
+    done
+    
+    rm -rf \${tmpdir}
   """
 }
