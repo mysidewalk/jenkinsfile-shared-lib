b1dcc7cf3fee48a7858803e23046cb9805daaf2d
Tony Ebel <tebel@mysidewalk.com>
2023-04-14 09:58:20 -0500

    [PFP-4187] pssh rewrite to use bash background processes and ssh (#15)

--- vars/pssh.groovy
+++ vars/pssh.groovy
@@ -2,12 +2,37 @@
 
 void call(Set<String> hosts, String command) {
   sh """
-    pssh \
-      -H "${hosts.sort().join(' ')}" \
-      -O StrictHostKeyChecking=no \
-      -O UserKnownHostsFile=/dev/null \
-      -t 300 \
-      --inline \
-      "${command}"
+    hosts="${hosts.sort().join(' ')}"
+    jobs=()
+    fail=0
+    tmpdir=$(mktemp -d)
+
+    for host in \${hosts[@]}; do
+      timeout 300 ssh \
+        -o StrictHostKeyChecking=no \
+        -o UserKnownHostsFile=/dev/null \
+        -o ConnectTimeout=10 \
+        -o BatchMode=yes \
+        \${host} \
+        "${command}" > \${tmpdir}/\${host} 2>&1 &
+
+        jobs+=(\$!)
+    done
+    
+    for pid in \${pids[@]}; do
+      wait \$pid || let "fail=1"
+    done
+    
+    for host in \${hosts[@]}; do
+      printf "\n\n#################\nOutput: \${host}\n#################\n"
+      cat \${tmpdir}/\${host}
+    done
+    
+    rm -rf \${tmpdir}
+
+    if (( fail != 0 )); then
+      echo "One or more ssh commands had a bad exit code"
+      exit 1
+    fi
   """
 }
