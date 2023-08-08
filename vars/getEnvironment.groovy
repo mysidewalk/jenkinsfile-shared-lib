82aa88ca6d9627ea22e29bded8c6ddbc7dbbbdb9
Jake Guthmiller <jguthmiller@mysidewalk.com>
2023-08-08 17:21:45 -0500

    Update getEnvironment.groovy

--- vars/getEnvironment.groovy
+++ vars/getEnvironment.groovy
@@ -4,6 +4,7 @@ String call(String action, String branchName) {
   BRANCH_TO_ENVIRONMENT = [
     (branch.EDGE): environment.EDGE,
     (branch.MASTER): environment.STAGE,
+    (branch.MAIN): environment.STAGE,
   ]
   if (action in [deploymentType.PROD_DEPLOY, deploymentType.PROD_PREDEPLOY]) {
     result = environment.PROD
