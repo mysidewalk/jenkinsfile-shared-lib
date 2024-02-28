9f7db600fd0bbba69c692a6a26582e56d4109f4b
Tony Ebel <tebel@mysidewalk.com>
2024-02-28 12:11:25 -0600

    cloud run deploy must have region param

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -1,15 +1,15 @@
 #!/usr/bin/env groovy
 
-void call(String serviceName, String image, String envfile) {
+void call(String serviceName, String image, String envfile, String region) {
   sh """
     set -o errexit
     set -o nounset
     set -o pipefail
 
     if [ -z "${envfile}" ]; then
-      gcloud run deploy ${serviceName} --image=${image} 
+      gcloud run deploy ${serviceName} --image=${image} --region=${region}
     else
-      gcloud run deploy ${serviceName} --image=${image} --env-vars-file=${envfile}
+      gcloud run deploy ${serviceName} --image=${image} --region=${region} --env-vars-file=${envfile}
     fi
   """
 }
