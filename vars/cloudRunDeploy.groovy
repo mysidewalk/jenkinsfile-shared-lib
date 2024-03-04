b970a3e696f15c1834599b2d8d39ade9d3a9f375
Tony Ebel <tebel@mysidewalk.com>
2024-03-04 14:17:13 -0600

    [PFP-5145] add dotenv2yaml and cloudRunDeploy functions (#28)

---
+++ vars/cloudRunDeploy.groovy
@@ -0,0 +1,15 @@
+#!/usr/bin/env groovy
+
+void call(String serviceName, String region, String image, String envfile) {
+  sh """
+    set -o errexit
+    set -o nounset
+    set -o pipefail
+
+    if [ -z "${envfile}" ]; then
+      gcloud run deploy ${serviceName} --image=${image} --region=${region}
+    else
+      gcloud run deploy ${serviceName} --image=${image} --region=${region} --env-vars-file=${envfile}
+    fi
+  """
+}
