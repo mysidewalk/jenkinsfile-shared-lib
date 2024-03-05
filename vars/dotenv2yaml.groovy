42e37c36a8584a8e9ed1d40ac8a6ebda68636c04
Tony Ebel <tebel@mysidewalk.com>
2024-03-04 19:12:23 -0600

    temp remove PORT etcd variable (#29)

--- vars/dotenv2yaml.groovy
+++ vars/dotenv2yaml.groovy
@@ -6,6 +6,8 @@ void call(String envfile, String outfile) {
     set -o nounset
     set -o pipefail
 
+    # temporarily remove PORT from envfile for Cloud Run
+    sed -i 's/^PORT=80//g' ${envfile}
     cat ${envfile} | docker run --rm -i gcr.io/mindmixer-sidewalk/dotenv-to-yaml > ${outfile}
   """
 }
