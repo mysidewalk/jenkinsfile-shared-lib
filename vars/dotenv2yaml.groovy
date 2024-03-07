f72bb3493b55061aa528d33668cbe19d1687957e
Tony Ebel <tebel@mysidewalk.com>
2024-03-07 13:21:05 -0600

    remove PORT env variable regardless of value (#30)

--- vars/dotenv2yaml.groovy
+++ vars/dotenv2yaml.groovy
@@ -7,7 +7,7 @@ void call(String envfile, String outfile) {
     set -o pipefail
 
     # temporarily remove PORT from envfile for Cloud Run
-    sed -i 's/^PORT=80//g' ${envfile}
+    sed -i '/^PORT=/d' ${envfile}
     cat ${envfile} | docker run --rm -i gcr.io/mindmixer-sidewalk/dotenv-to-yaml > ${outfile}
   """
 }
