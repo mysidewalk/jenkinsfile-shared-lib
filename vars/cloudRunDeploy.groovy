3236b8c86689977f4a603d6fbf70e04ec0932e7b
Tony Ebel <tebel@mysidewalk.com>
2024-03-27 13:54:28 -0500

    [PFP-5276] Cloud Run Labels (#32)

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -1,15 +1,24 @@
 #!/usr/bin/env groovy
 
-void call(String serviceName, String region, String image, String envfile) {
+void call(String serviceName, String region, String image, String envfile='') {
   sh """
     set -o errexit
     set -o nounset
     set -o pipefail
 
     if [ -z "${envfile}" ]; then
-      gcloud run deploy ${serviceName} --image=${image} --region=${region}
+      gcloud run deploy ${serviceName} \
+        --quiet \
+        --image=${image} \
+        --update-labels=managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --region=${region}
     else
-      gcloud run deploy ${serviceName} --image=${image} --region=${region} --env-vars-file=${envfile}
+      gcloud run deploy ${serviceName} \
+        --quiet \
+        --image=${image} \
+        --update-labels=managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --region=${region} \
+        --env-vars-file=${envfile}
     fi
   """
 }
