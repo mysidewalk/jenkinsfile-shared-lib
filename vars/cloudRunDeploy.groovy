8a082711ce5933e00b7349abaa9eafcc32764aed
Tony Ebel <tebel@mysidewalk.com>
2024-03-27 08:20:21 -0500

    quiet and update-labels params added to cloudRunDeploy function

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -7,9 +7,18 @@ void call(String serviceName, String region, String image, String envfile) {
     set -o pipefail
 
     if [ -z "${envfile}" ]; then
-      gcloud run deploy ${serviceName} --image=${image} --region=${region}
+      gcloud run deploy ${serviceName} \
+        --quiet \
+        --image=${image} \
+        --update-labels managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --region=${region}
     else
-      gcloud run deploy ${serviceName} --image=${image} --region=${region} --env-vars-file=${envfile}
+      gcloud run deploy ${serviceName} \
+        --quiet \
+        --image=${image} \
+        --update-labels managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --region=${region} \
+        --env-vars-file=${envfile}
     fi
   """
 }
