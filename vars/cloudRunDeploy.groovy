f1c5c86b0d94f741d6aae13b19ae51de6907cc72
Tony Ebel <tebel@mysidewalk.com>
2024-03-29 11:19:58 -0500

    grab git commit hash using rev-parse

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -6,11 +6,13 @@ void call(String serviceName, String region, String image, String envfile='') {
     set -o nounset
     set -o pipefail
 
+    GIT_REVISION=\$(git rev-parse HEAD)
+
     if [ -z "${envfile}" ]; then
       gcloud run deploy ${serviceName} \
         --quiet \
         --image=${image} \
-        --update-labels=managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --update-labels=managed-by=jenkins,commit-sha=\$GIT_REVISION \
         --region=${region}
     else
       gcloud run deploy ${serviceName} \
