0cc1517049215f83c33c4c037da74b8914ae317b
Tony Ebel <tebel@mysidewalk.com>
2024-04-30 10:06:32 -0500

    [PFP-5276] Allow passing extra labels for cloudRunDeploy (#33)

--- vars/cloudRunDeploy.groovy
+++ vars/cloudRunDeploy.groovy
@@ -1,22 +1,28 @@
 #!/usr/bin/env groovy
 
-void call(String serviceName, String region, String image, String envfile='') {
+void call(String serviceName, String region, String image, String envfile='', String extraLabels='') {
   sh """
     set -o errexit
     set -o nounset
     set -o pipefail
 
+    labels="mangedby=jenkins"
+
+    if [ ! -z "${extraLabels}" ]; then
+      labels+=",${extraLabels}"
+    fi
+
     if [ -z "${envfile}" ]; then
       gcloud run deploy ${serviceName} \
         --quiet \
         --image=${image} \
-        --update-labels=managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --update-labels=\${labels} \
         --region=${region}
     else
       gcloud run deploy ${serviceName} \
         --quiet \
         --image=${image} \
-        --update-labels=managed-by=jenkins,commit-sha=${env.GIT_COMMIT} \
+        --update-labels=\${labels} \
         --region=${region} \
         --env-vars-file=${envfile}
     fi
