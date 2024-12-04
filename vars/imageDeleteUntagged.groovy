319f1cefc7a403ade8edd8a6fd7cf0d3f35cac23
Tony Ebel <tebel@mysidewalk.com>
2024-12-03 20:25:14 -0600

    leave untagged images in GAR (#36)

--- vars/imageDeleteUntagged.groovy
+++ vars/imageDeleteUntagged.groovy
@@ -4,9 +4,4 @@ void call(String image) {
   echo "Deleting untagged images of ${image}."
   // Filtering is done on image and then dangling on that subset, potentially resulting in not truly dangling images
   sh "docker images '${image}' -qf 'dangling=true' | xargs --no-run-if-empty docker rmi || true"
-  sh """
-    gcloud container images list-tags ${image} --filter='NOT tags:*' --format=json \
-      | jq '.[].digest' \
-      | xargs --no-run-if-empty -I {} gcloud container images delete -q ${image}@{}
-  """
 }
